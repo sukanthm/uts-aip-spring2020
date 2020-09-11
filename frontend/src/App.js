3091e7233a72c20c0faf1d60eacee8f646949a4b
Pranshu Midha <pranshu1995@gmail.com>
2020-09-11 17:40:16 +1000

    👌Improve Signup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,29 +9,7 @@ class App extends React.Component {
   constructor(props){
     super(props);
   }
-    // this.state = {
-    //   count: 0
-    // }
-    // this.getInit();
-  // }
 
-  // componentDidMount = () => {
-  //   this.getInit();
-  // }
-
-  //  getInit = async () => {
-  //   let result = await fetch("/api/count");
-  //   let json = await result.json();
-  //   this.setState({count: json.count});
-  //   // this.setState({count: this.state.count + 1})
-  // }
-
-  // increment = async() => {
-  //   let result = await fetch("/api/inc", {method: "POST", body:{"count": this.state.count}});
-  //   let json = await result.json();
-  //   this.setState({count: json.count});
-  // }
-  
   render(){
   return (
     <Signup></Signup>
