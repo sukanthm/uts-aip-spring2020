5a5cf4ae4d2733af9dc49fa8d64c05690a4ff5d7
harris-ingram <02079655@student.uts.edu.au>
2020-09-15 22:09:40 +1000

    🐛 FIX: Update package and resolve installation

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,1 +1,11 @@
 import Header from '../template-parts/Header';
+const Dashboard = (props) => {
+    return(
+        <>
+            <Header />
+            <div className="dashboard-page"></div>
+        </>
+    );
+}
+
+export default Dashboard;
