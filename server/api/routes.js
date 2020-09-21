058e662094bd28820548a050a29e36da2d857144
sukanth <sukanth@yahoo.com>
2020-09-21 05:12:39 -0700

    👌 IMPROVE: add functionality and refactor signup and login api

--- server/api/routes.js
+++ server/api/routes.js
@@ -2,6 +2,36 @@ import fpUser from '../persistence/objects/fpUser';
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+async function custom_hash(input){
+    return new Promise((resolve, reject) => {
+        bcrypt.hash(input, saltRounds, function(err, hash) {
+          if (err) reject(err);
+          resolve(hash);
+        });
+    });
+}
+
+async function is_password_valid(plaintextPassword, hashedPassword){
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(plaintextPassword, hashedPassword, function(err, result) {
+        if (err) reject(err);
+        resolve(result);
+        });
+    });
+}
+
+function get_req_headers(req, headers) {
+    let header, output = [];
+    for (let i = 0, len = headers.length; i < len; i++) {
+        header = req.header(headers[i]);
+        if (header === undefined || header === 'null'){
+            return [false, headers];
+        }
+        output.push(header);
+    }
+    return [true, output];
+}
+
 module.exports = function(app){
 
     app.get('/signup', function(req, res){
@@ -10,23 +40,18 @@ module.exports = function(app){
 
     app.post('/signup', async function(req, res){
         /*
-        req headers:
+        request headers:
             email (string): regex checked in react
             password (string): plaintext
             name (string)
-        res headers:
+        response headers:
             success (bool)
             message (string)
         TODO:
-            add checks for empty input strings and test flow
+
         */
-        let email, password, name;
-        try {
-            email = req.header('email');
-            password = req.header('password');
-            name = req.header('name');
-            //add checks for empty strings
-        } catch (error) {
+        let [successFlag, [email, password, name]] = get_req_headers(req, ['email', 'password', 'name']);
+        if (!successFlag){
             console.error('mandatory request headers missing');
             res.set('success', false);
             res.set('message', 'mandatory request headers missing');
@@ -39,22 +64,15 @@ module.exports = function(app){
         });
 
         if (created){
-            const hashedPassword = await new Promise((resolve, reject) => {
-                bcrypt.hash(password, saltRounds, function(err, hash) {
-                  if (err) reject(err);
-                  resolve(hash);
-                });
-            });
-            newUser.password = hashedPassword;
+            newUser.password = await custom_hash(password);
             newUser.name = name;
-
             await newUser.save();
 
             res.set('success', true);
             res.set('message', 'user created');
             res.redirect(200, '/login');
         }
-        else {
+        else {;
             console.info(email + ' already exists');
             res.set('success', false);
             res.set('message', 'email already exists');
@@ -64,22 +82,18 @@ module.exports = function(app){
 
     app.get('/login', async function(req, res){
         /*
-        req headers:
+        request headers:
             email (string): regex checked in react
             password (string): plaintext
-        res headers:
+        response headers:
             success (bool)
             message (string)
-            token (string)
+            cooke with login token (string)
         TODO:
-            add checks for empty input strings and test flow
+
         */
-        let email, password, name;
-        try {
-            email = req.header('email');
-            password = req.header('password');
-            //add checks for empty strings
-        } catch (error) {
+        let [successFlag, [email, password]] = get_req_headers(req, ['email', 'password']);
+        if (!successFlag){
             console.error('mandatory request headers missing');
             res.set('success', false);
             res.set('message', 'mandatory request headers missing');
@@ -88,42 +102,30 @@ module.exports = function(app){
         }
 
         const user = await fpUser.findOne({
-            where: {email: email},
+           where: {email: email},
         });
 
         if (user === null) {
             console.info(email + ' not registered');
             res.set('success', false);
             res.set('message', 'email not registered');
-            res.redirect(401, '/signup');
+            res.redirect(406, '/signup');
             return;
         } 
         else {
-            const is_valid_password = await new Promise((resolve, reject) => {
-                bcrypt.compare(password, user.password, function(err, result) {
-                if (err) reject(err);
-                resolve(result);
-                });
-            });
-            if (!is_valid_password) {
+            const isPasswordValid = await is_password_valid(password, user.password);
+            if (!isPasswordValid) {
                 res.set('message', 'incorrect email password combo');
                 res.set('success', false);
                 res.redirect(401, '/login');                
             } 
             else {
-                const cookie = req.cookies.aip_fp;
-                if (cookie === undefined) {
-                    const login_token = await new Promise((resolve, reject) => {
-                        bcrypt.hash(user.id.toString(), saltRounds, function(err, hash) {
-                        if (err) reject(err);
-                        resolve(hash);
-                        });
-                    });
-                    res.cookie('aip_fp', login_token, {maxAge: 3600});
-                    res.set('message', 'login token generated and cookie set');
-                    res.set('success', true);
-                    res.redirect(200, '/');
-                } 
+                //const cookie = req.cookies.aip_fp;
+                const loginToken = await custom_hash(user.id.toString());
+                res.cookie('aip_fp', loginToken, {maxAge: 3600});
+                res.set('message', 'login token generated and cookie set');
+                res.set('success', true);
+                res.redirect(200, '/');
             }
         }
     });
