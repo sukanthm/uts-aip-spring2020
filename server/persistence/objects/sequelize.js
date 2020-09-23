42ff787ddca4fbe11d22baafd2223b9ef0e51144
harris-ingram <02079655@student.uts.edu.au>
2020-09-24 03:11:07 +1000

    🐛 FIX: Removing credentials from git and cleaning up package

--- server/persistence/objects/sequelize.js
+++
@@ -1,7 +0,0 @@
-export const Sequelize = require('sequelize');
-export const sequelize = new Sequelize(
-    'forward_pay',  // Database name
-    'postgres',        // Username
-    'aip-dhps',        // Password
-    {dialect: 'postgres', host: 'localhost'}
-);
