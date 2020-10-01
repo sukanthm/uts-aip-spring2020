8e70a8ce08000c9c13c6839a77a455eba26050e4
Dilak-Shakya <13758418@student.uts.edu.au>
2020-10-01 16:17:06 +1000

    👌IMPROVE: Design task detail page

--- frontend/pages/task/[taskId]/index.js
+++ frontend/pages/task/[taskId]/index.js
@@ -13,73 +13,84 @@ const TaskId = () => {
     let upTaskReward = () => {
         console.log("Upping it!");
     }
-    
+
     //Sample JSON data
     let taskData = {
-        title: "Task 1",
-        desc: "Description 1",
-        img: "../../images/outbox.png",
+        title: "Clean the Fridge",
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget nulla sed purus sodales auctor ultrices convallis metus. Integer tincidunt eros eu metus sollicitudin sodales. Vestibulum vel tellus hendrerit, dignissim risus nec, tincidunt mauris. Ut nunc turpis, fermentum venenatis sodales facilisis, interdum vel risus.",
+        img: "../../images/fridge.jpg",
         rewardsData: [
             {
                 "User 1": {
-                coffee: 2,
-                snacks: 1
+                    coffee: 2,
+                    snacks: 1
                 }
             },
             {
                 "User 2": {
-                meal: 1,
-                drink: 3
+                    meal: 1,
+                    drink: 3
                 }
             }
         ],
         createdBy: "User 2",
         createdAt: "08/06/2020"
-        };
+    };
 
-        let individualUser = taskData.rewardsData.map((key) => 
-            {   
-                // Iterate through each user
-                let keyVal = Object.keys(key)[0];    // Get value of the name of the user
-                return <IndividualRewardCard user={keyVal} rewards={key[keyVal]}></IndividualRewardCard>
-            }
-        )
+    let individualUser = taskData.rewardsData.map((key) => {
+        // Iterate through each user
+        let keyVal = Object.keys(key)[0];    // Get value of the name of the user
+        return <IndividualRewardCard user={keyVal} rewards={key[keyVal]}></IndividualRewardCard>
+    }
+    )
 
-        return(
-            <>
+    return (
+        <>
             <Header></Header>
             <div className="task-new-class">
-            <div className="container-fluid">
-                <div className="row">
-                    <div className="col-md-2 task-image-holder">
-                        
-                        <img src={taskData.img} alt="Task Image" className="task-image container"></img>
-                        
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-3 task-image-holder">
+
+                            <img src={taskData.img} alt="Task Image" className="task-image container"></img>
+
+                        </div>
+                        <div className="col-md-9">
+
+                            <h2 className="forward-page-header">{taskData.title}</h2>
+                            <p>{taskData.desc}</p>
+                            <p>Created by <strong>{taskData.createdBy}</strong> at {taskData.createdAt}</p>
+                        </div>
                     </div>
-                    <div className="col-md-10">
-                        
-                        <h2>{taskData.title}</h2>
-                        <p>{taskData.desc}</p>
-                        <p>Created by <strong>{taskData.createdBy}</strong> at {taskData.createdAt}</p>
+                    <hr/>
+
+                    <div className="row">
+                        <div className="col-md-12 contributors">
+                            <h4>Contributors for this Task</h4>
+                        </div>
+                    </div>
+                    <div className="row">
+                        {individualUser}
+                    </div>
+
+                    <div className="row">
+                    <div className="col-md-12">
+                        <button className="btn btn-primary right  btn-forward-main" onClick={() => claimTask()}>Claim Task</button>
+                        <button className="btn btn-outline-primary mr-3 right btn-forward-main" onClick={() => upTaskReward()}>Add Reward Task</button>
                     </div>
                 </div>
-            </div>
-
-            <div className="container-fluid">
-                <div className="row">
-                    {individualUser}
                 </div>
+
+                
+
+
+
             </div>
 
-            
-            <button className="btn btn-outline-primary" onClick={() => upTaskReward()}>Add Reward Task</button>
-            <button className="btn btn-primary right" onClick={() => claimTask()}>Claim Task</button>
-        </div>
-            
-            </>
-        )
+        </>
+    )
 
-        
-    }
 
-    export default TaskId;
+}
+
+export default TaskId;
--- frontend/template-parts/TopNavbar.js
+++ frontend/template-parts/TopNavbar.js
@@ -11,7 +11,7 @@ const TopNavbar = (props) => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Dashboard <span className="sr-only">(current)</span></a>
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Favours</a>
@@ -21,6 +21,9 @@ const TopNavbar = (props) => {
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Leaderboard</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Party</a>
           </li>
         </ul>
         <form className="form-inline my-2 my-lg-0">
