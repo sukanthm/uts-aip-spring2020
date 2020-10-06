909794e0163438ab501151129554920668617a4a
sukanth <sukanth@yahoo.com>
2020-10-06 00:20:17 -0700

    👌 IMPROVE: change image upload dest to frontend, fix upload filename extensions

--- server/api/favor.js
+++ server/api/favor.js
@@ -8,11 +8,11 @@ var multer  = require('multer');
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname + '/../public_images/'));
+        cb(null, path.join(__dirname + '/../../frontend/public/proof_images/'));
     },
     filename: function (req, file, cb) {
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, file.fieldname + '-' + uniqueSuffix)
+      cb(null, uniqueSuffix + '-' + file.originalname)
     }
   })
 var upload = multer({ storage: storage })
