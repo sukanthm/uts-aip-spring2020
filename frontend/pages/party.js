5b7b3e7b66e8a1d515a08ff044660f3e7ab76a39
Dilak-Shakya <13758418@student.uts.edu.au>
2020-10-15 22:58:45 +1100

    👌IMPROVE: Add Party Page

--- frontend/pages/party.js
+++ frontend/pages/party.js
@@ -1,67 +1,79 @@
-import {useEffect, useState, useRef} from 'react';
+import { useEffect, useState, useRef } from 'react';
 import Header from '../template-parts/Header';
 import { useRouter } from 'next/router';
+import helpers from '../functions/helpers.js';
 const Party = (props) => {
     const itemsPerPage = 5;
     const currentPage = useRef(0);
     const router = useRouter();
+    const [partyData, setPartyData] = useState({});
 
-    const [taskRows, setTaskRows] = useState([]);
-    const [moreScroll, setMoreScroll] = useState(true);
-
-    const fetchTasks = async(status, currentPage, itemsPerPage, search) => {
-        try{
-        let fetchJson = {
-                dashboardFilter: "Creator",
-                requestStatus: status,
-                currentPage: currentPage,
-                itemsPerPage: itemsPerPage,
-                searchData: search
-            }
-        let result = await fetch("/api/party", {method: "GET"});
+    const fetchTasks = async () => {
+        try {
+            let result = await fetch("/api/party", { method: "GET" });
             let json = await result.json();
             console.log("kya?", json);
-            if(json.output.currentPage == (json.output.totalPages-1)){
-                console.log("ruk gaya");
-                setMoreScroll(false);
-            }
-        console.log("here");
-            // let arr = taskRows;
-            const arr = [...taskRows];
-            json.output.rows.map((row) => {
-                arr.push(row);
-            })
-            console.log("arro", arr);
-            console.log("rumba", currentPage);
-            setTaskRows(arr);
+            setPartyData(json.output);
         }
-        catch(err){
+        catch (err) {
             console.log(err);
         }
-        }
+    }
 
-
-        useEffect(() => {fetchTasks("All", currentPage.current, itemsPerPage, "")}, []);
-
+    useEffect(() => { fetchTasks("All", currentPage.current, itemsPerPage, "") }, []);
 
     return (
         <>
             <Header />
             <div className="container">
                 <div className="row">
-                    <div className="party-container container-fluid">
+                    {
+                        Object.keys(partyData).map((key) => {
+                            return (
+                                <div className="col-md-6">
+                                    <div className="party-container">
+                                    <div className="task-des">
+                                        {/* Printing name of the reward */}
+                                        <h4><span>Party Detected for reward:</span> <b>{helpers.rewardTitle(key)}</b></h4>
+                                    </div>
+                                    <hr/>
+                                    <div>
+                                        {
+                                            // looping through party data and showing the people involved in the party
+                                            partyData[key].map((row, index) => {
+                                                return (
+                                                    <div>
+                                                        <b>People for Party No: {index+1}</b>
+                                                        <p>{row.join()}</p>
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </div>
+                                    </div>
+                                </div>
+                            )
+
+                            console.log("display temp:", helpers.rewardTitle(key));
+                            partyData[key].map((row, index) => {
+                                console.log("this is row:", row);
+                                console.log(row.join());
+                            })
+                        })
+                    }
+                    {/* <div className="party-container container-fluid">
                         <div className="task-des">
                             <b>Party Detected!</b>
                         </div>
-                            <br/>
+                        <br />
                         <div>
                             <b>People Involved in the party</b>
-                            <br/>
+                            <br />
                             <div>Person 1</div>
                             <div>Person 1</div>
                             <div>Person 1</div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </>
